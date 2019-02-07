d89edcd36478afb71800fdaa08ac70b51078cee0
Atsushi Nagase <a@ngs.io>
2019-02-07 11:53:01 +0900

    Update CLI

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -2,25 +2,27 @@
 
 set -eux
 
-UPLOAD_URL=$(cat $GITHUB_EVENT_PATH | jq -r .upload_url)
-UPLOAD_URL=${UPLOAD_URL/{?name,label}/}
-RELEASE_NAME=$(cat $GITHUB_EVENT_PATH | jq -r .name)
+EVENT_DATA=$(cat $GITHUB_EVENT_PATH)
+echo $EVENT_DATA | jq .
+UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
+UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
+RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
 PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 NAME="${PROJECT_NAME}_${RELEASE_NAME}_${GOOS}_${GOARCH}"
 
 tar cvfz tmp.tgz $PROJECT_NAME
-CKECKSUM=$(md5sum tmp.tgz | cut -d ' ' -f 1)
+CHECKSUM=$(md5sum tmp.tgz | cut -d ' ' -f 1)
 
 curl \
   -X POST \
   --data-binary @tmp.tgz \
   -H 'Content-Type: application/gzip' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${$NAME}.tar.gz"
+  "${UPLOAD_URL}?name=${NAME}.tar.gz"
 
 curl \
   -X POST \
   --data $CHECKSUM \
   -H 'Content-Type: text/plain' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${$NAME}_checksum.txt"
+  "${UPLOAD_URL}?name=${NAME}_checksum.txt"
